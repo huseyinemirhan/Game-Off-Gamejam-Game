009a3976a47b392b7dee96a5fe337c464fee5353
huseyinemirhan <86725368+huseyinemirhan@users.noreply.github.com>
2022-11-06 21:07:25 +0300

    sprites and random generator

---
+++ Cut!/Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    //1 ----> bottom
+    //2 ----> top
+    //3 ----> left
+    //4 ----> right
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+    public float waitTime = 4f;
+
+    void Start()
+    {
+        Destroy(gameObject,waitTime);
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    void Spawn()
+    {
+        if (spawned== false)
+        {
+            if (openingDirection == 1)
+            {
+                //Need to spawn top door
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 2)
+            {
+                //Need to spawn bottom door 
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 3)
+            {
+                //Need to spawn right door
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 4)
+            {
+                //Need to spawn left door
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, Quaternion.identity);
+            }
+        }
+        spawned = true;
+       
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("SpawnPoint") == true)
+        {
+            if(other.GetComponent<RoomSpawner>().spawned == false && spawned == false)
+            {
+                //spawn a wall
+                Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            spawned = true;
+             
+           
+        }
+    }
+
+}
---
+++ Cut!/Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] bottomRooms;
+    public GameObject[] topRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] rightRooms;
+
+    public GameObject closedRoom;
+    public List<GameObject> rooms;
+
+    public float waitTime;
+    private bool spawnedBoss;
+    public GameObject boss;
+
+    void Update()
+    {
+        if(waitTime <= 0 && spawnedBoss == false)
+        {
+           if(rooms[rooms.Count-1].tag == "ClosedRoom")
+           {
+                Instantiate(boss,rooms[rooms.Count-2].transform.position, Quaternion.identity);
+           }else{
+                 Instantiate(boss,rooms[rooms.Count-1].transform.position, Quaternion.identity);
+           }
+          
+           spawnedBoss = true;
+        } else{
+          waitTime -= Time.deltaTime;  
+        }
+    }
+}
